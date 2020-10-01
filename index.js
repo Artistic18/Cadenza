77f1154f969429db7b5b2c0ad8bf37656ab3f564
Artistic18 <hrishitbiswas20@gmail.com>
2020-10-01 21:10:35 +0530

    added bot func

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const Discord = require('discord.js');
+const{
+    prefix,
+    token,
+} = require('./config.json');
+
 const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Bot Online');
+})
+
+client.login(token);
 
