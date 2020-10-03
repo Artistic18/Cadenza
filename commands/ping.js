b24f3a188288639ba77e9a92e2d77416d794da89
Artistic18 <hrishitbiswas20@gmail.com>
2020-10-04 03:21:39 +0530

    added bulk delete module

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: "This is a ping command",
     execute(message, args){
-        message.channel.send('Hi');
-    }
-}
+        message.channel.send(`Hello ${message.author.username}`);
+    },
+};
--- index.js
+++ index.js
@@ -23,11 +23,30 @@ client.on('message', msg=> {
     if(!msg.content.startsWith(prefix) || msg.author.bot){
         return;
     }
-    const args = msg.content.slice(prefix.length).split(/ +/);
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
        client.commands.get('ping').execute(msg, args);
+    }
+    else if(command === 'userinfo'){
+        client.commands.get('userinfo').execute(msg,args);
+    }
+    else if(command === 'delete'){
+        const amount = parseInt(args[0]) +1 ;
+        if(isNaN(amount)){
+            return msg.reply('Enter a valid number.');
+        }
+        else if(amount <= 1 || amount > 100){
+            return msg.reply('Enter a number between 1 and 99.');
+        }
+        else{
+        msg.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            msg.channel.send('There was an error trying to delete messages');
+        });
+        msg.channel.send(`Deleted ${args} messages.`);
+        }
     }
 });
 
