82def7ee1ddea85affc1d3991ec58023966b692f
Hrishit Prasad Biswas <hrishitbiswas20@gmail.com>
2020-10-06 00:09:19 +0530

    Update play.js

--- include/play.js
+++ include/play.js
@@ -18,7 +18,7 @@ module.exports = {
         }
         queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));
         const dispatcher = queue.connection
-         .play(ytdlDiscord(song.url), {bitrate: 32000})
+         .play(ytdlDiscord(song.url))
          .on("finish", () => {
              queue.songs.shift();
              module.exports.play(queue.songs[0], message);
