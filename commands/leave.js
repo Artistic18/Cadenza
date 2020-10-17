d5780a3a34ef3ab76867acfc1715390e12b01781
Artistic18 <hrishitbiswas20@gmail.com>
2020-10-18 00:31:16 +0530

    fixed not leaving when queue empty

--- commands/leave.js
+++ commands/leave.js
@@ -3,8 +3,6 @@ module.exports = {
     description: "Disconnects the bot.",
     execute(message){
         const queue = message.client.queue.get(message.guild.id);
-        if(!queue)
-          return message.reply("Bot is not in a voice channel!");
         queue.songs = []
         queue.channel.leave();
         message.channel.send(`**Cadenza** left successfully`).catch(console.error);
